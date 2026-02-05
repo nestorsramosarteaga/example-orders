05f0a89509da58663df21dc1d36c80e9c3431195
Nestor Ramos <nestorsramosarteaga@gmail.com>
2026-02-05 09:36:45 -0500

    add astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless'; // Or static if you don't use SSR
+
 export default defineConfig({
+  adapter: vercel({}), 
+  output: 'server', // This allows Astro to handle dynamic requests on Vercel or static
   vite: {
     plugins: [tailwindcss()]
   }
